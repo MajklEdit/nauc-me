e579435f29da2bfa1788a5b4b3d23e78b78ce7f0
MajklEdit <michaelworkedit@gmail.com>
2026-04-05 10:33:34 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,7 +5,6 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const SUPABASE_URL = 'https://TVUJ-PROJEKT.supabase.co'   // ← sem doplň
-const SUPABASE_KEY = 'TVUJ-ANON-KEY'                       // ← sem doplň
-
+const SUPABASE_URL = 'https://dxibiwizupnnmsdqovee.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4aWJpd2l6dXBubm1zZHFvdmVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyNDI5NDMsImV4cCI6MjA5MDgxODk0M30.KEdRUbKDqfwWooeCeuHNDScBQWGp7c7R9VGv8lWHsaY'
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
